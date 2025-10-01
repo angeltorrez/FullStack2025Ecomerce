9564257465e120b6d41b46863aefde1e6a28d583
Angel Torrez <angeltorrez113@gmail.com>
2025-10-01 13:57:32 -0300

    fix toggleTheme

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,16 +1,24 @@
 const temaOscuro = ()=>{
-  document.body.setAttribute("data-bs-theme","dark");
+  document.documentElement.setAttribute("data-bs-theme","dark");
   document.getElementById("moonIco").setAttribute("class","d-block");
   document.getElementById("sunIco").setAttribute("class","d-none");
 }
 const temaClaro= ()=>{
-  document.body.setAttribute("data-bs-theme","light");
+  document.documentElement.setAttribute("data-bs-theme","light");
   document.getElementById("moonIco").setAttribute("class","d-none");
   document.getElementById("sunIco").setAttribute("class","d-block");
 }
 const toggleTema = () =>{
-  document.body.getAttribute("data-bs-theme")=== "light" ? temaOscuro() : temaClaro();
+  if (document.documentElement.getAttribute("data-bs-theme")=== "light") {
+    temaOscuro();
+    localStorage.setItem("theme","dark");
+  }
+  else{
+    temaClaro();
+    localStorage.setItem("theme","light");
+  }
 }
+
 /*let getTheme = () =>{
   return localStorage.getItem("theme") ||
   (window.matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light");
